81734e387d2485efe45f42399081a254fbf4295a
yamadapc <tacla.yamada@gmail.com>
2015-11-25 18:32:18 -0200

    Refactor extract-dependencies

--- extract-dependencies/src/Main.hs
+++ extract-dependencies/src/Main.hs
@@ -2,44 +2,36 @@
 
 module Main where
 
-import           Control.Lens                          ((&), (.~), (^.), (^..))
-import           Control.Monad                         (forM_)
-import           Data.Aeson.Lens                       (key, values, _Integral)
-import           Data.ByteString.Lazy.Char8            (ByteString)
-import qualified Data.ByteString.Lazy.Char8            as L
-import           Distribution.Package                  (Dependency (..),
-                                                        unPackageName)
-import           Distribution.PackageDescription       (condLibrary,
-                                                        condTreeConstraints)
-import           Distribution.PackageDescription.Parse (ParseResult (..),
-                                                        parsePackageDescription)
-import           Network.Wreq
-import           System.Environment                    (getArgs)
+import           Control.Concurrent.Async
+import           Control.Monad
+import qualified Data.Set                               as Set
+import           Distribution.Package
+import           Distribution.PackageDescription
+import           Distribution.PackageDescription.Parse
+import           Distribution.PackageDescription.Remote
+import           System.Environment
 
-getLatestRevision :: String -> IO Integer
-getLatestRevision package =
-  maximum . (^.. responseBody . values . key "number" . _Integral) <$> getWith opt uri
-  where opt = defaults & header "Accept" .~ ["application/json"]
-        uri = "http://hackage.haskell.org/package/" ++ package ++ "/revisions/"
-
-getPackageRevision :: String -> Integer -> IO ByteString
-getPackageRevision package revision =
-  (^. responseBody) <$> get uri
-  where uri = "http://hackage.haskell.org/package/" ++ package ++ "/revision/" ++ show revision
-
-parseCabal :: ByteString -> Either String [String]
-parseCabal file =
-  case parsePackageDescription (L.unpack file) of
-      ParseOk _ info -> case condLibrary info of
-                            Just lib -> Right $ dependencyName <$> condTreeConstraints lib
-                            Nothing -> Left "Package is not a library."
-      ParseFailed e -> Left $ show e
-  where dependencyName (Dependency name _) = unPackageName name
+extractDependencies :: String -> IO [String]
+extractDependencies npkg = do
+    ppkg <- getPackageLatest npkg
+    case ppkg of
+        ParseOk _ info ->
+            case condLibrary info of
+                Just lib -> return $ dependencyName <$> condTreeConstraints lib
+                Nothing -> error "Package is not a library"
+        _ -> error "Failed to parse package description"
+  where
+    dependencyName (Dependency name _) = unPackageName name
 
 main :: IO ()
 main = do
-  package <- head <$> getArgs
-  getLatestRevision package >>= getPackageRevision package >>= display . parseCabal
-  where display ds = case ds of
-                         Left e -> error e
-                         Right ds' -> forM_ ds' putStrLn
+    npkgs <- getArgs
+    deps <- mapConcurrently extractDependencies npkgs
+    forM_ (uniq (concat deps)) putStrLn
+  where
+      uniq = uniq' Set.empty
+        where
+          uniq' _ [] = []
+          uniq' x (y:ys) = if Set.member y x
+                           then uniq' x ys
+                           else y : uniq' (Set.insert y x) ys
