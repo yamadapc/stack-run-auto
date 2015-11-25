aff97c923c5ed45fc5a08bf905796a55ef7446b3
asakeron <asakeron@gmail.com>
2015-11-25 10:27:13 -0200

    download cabal file from Hackage for a given package and revision

--- extract-dependencies/src/Main.hs
+++ extract-dependencies/src/Main.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Control.Lens       ((&), (.~), (^..))
+import           Control.Lens       ((&), (.~), (^..), (^.))
 import           Data.Aeson.Lens    (key, values, _Integral)
 import           Network.Wreq
 import           System.Environment (getArgs)
+import           Data.ByteString.Lazy.Char8 (ByteString)
 
 getLatestRevision :: String -> IO Integer
 getLatestRevision package =
@@ -12,5 +13,12 @@ getLatestRevision package =
   where opt = defaults & header "Accept" .~ ["application/json"]
         uri = "http://hackage.haskell.org/package/" ++ package ++ "/revisions/"
 
+getPackageRevision :: String -> Integer -> IO ByteString
+getPackageRevision package revision =
+  (^. responseBody) <$> get uri
+  where uri = "http://hackage.haskell.org/package/" ++ package ++ "/revision/" ++ show revision
+
 main :: IO ()
-main = getArgs >>= getLatestRevision . head >>= print
+main = do
+  package <- head <$> getArgs
+  getLatestRevision package >>= getPackageRevision package >>= print
