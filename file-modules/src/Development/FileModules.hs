cab65064c009b95db18b474a5e0d2c49223a7f02
yamadapc <tacla.yamada@gmail.com>
2015-11-25 22:56:28 -0200

    Trade efficiency for less bugs

--- file-modules/src/Development/FileModules.hs
+++ file-modules/src/Development/FileModules.hs
@@ -2,34 +2,29 @@
 module Development.FileModules where
 
 import           Control.Concurrent.Async (mapConcurrently)
-import           Control.Concurrent.STM
 import           Control.Monad            (forM)
 import           Data.String.Utils        (split)
 import           Language.Haskell.Exts    (ImportDecl (..),
                                            ModuleHeadAndImports (..),
                                            ModuleName (..), NonGreedy (..),
                                            ParseResult (..), parse)
-import qualified STMContainers.Set        as Set
 import           System.Directory
 import           System.FilePath
 
-fileModulesRecur :: Set.Set String -> FilePath -> IO [String]
-fileModulesRecur seen fname = do
-    modules <- fileModules fname
-    modules' <- flip mapConcurrently modules $ \m -> do
-        let pth = takeDirectory fname </> joinPath (split "." m) ++ ".hs"
-        isSeen <- atomically $ Set.lookup pth seen
-        if isSeen
-           then return [m]
-           else do
-               atomically $ Set.insert pth seen
-               isLocalModule <- doesFileExist pth
-               if isLocalModule
-                   -- If we're hitting a local modules, ignore it on the
-                   -- output (this may not be what we want)
-                   then fileModulesRecur seen pth
-                   else return [m]
-    return (concat modules')
+fileModulesRecur :: FilePath -> IO [String]
+fileModulesRecur fname = run fname
+  where
+    run f = do
+      modules <- fileModules f
+      modules' <- flip mapConcurrently modules $ \m -> do
+          let pth = takeDirectory fname </> joinPath (split "." m) ++ ".hs"
+          isLocalModule <- doesFileExist pth
+          if isLocalModule
+              -- If we're hitting a local modules, ignore it on the
+              -- output (this may not be what we want)
+              then run pth
+              else return [m]
+      return (concat modules')
 
 fileModules :: FilePath -> IO [String]
 fileModules fname = do
