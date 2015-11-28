8f0bd08d4e355aac3dabde2e4192e5cee54247a7
yamadapc <tacla.yamada@gmail.com>
2015-11-28 19:42:50 -0200

    Add `file-modules` `--version` and `--help`

--- file-modules/src/Main.hs
+++ file-modules/src/Main.hs
@@ -5,9 +5,17 @@ import           Control.Monad            (forM_)
 import           Data.List.Utils          (uniq)
 import           Development.FileModules
 import           System.Environment       (getArgs)
+import           System.Exit              (exitFailure)
 
 main :: IO ()
 main = do
     args <- getArgs
-    modules <- mapConcurrently fileModulesRecur args
-    forM_ (uniq (concat modules)) putStrLn
+    case args of
+      "--version":_ -> putStrLn "0.1.1.0"
+      "--help":_ -> printUsage
+      [] -> printUsage >> exitFailure
+      ms -> do
+        modules <- mapConcurrently fileModulesRecur ms
+        forM_ (uniq (concat modules)) putStrLn
+  where
+    printUsage = putStrLn "Usage: file-modules <files...>"
