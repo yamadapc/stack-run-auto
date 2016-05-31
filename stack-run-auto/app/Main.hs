c6c27e8e2fe339ca336040007f5a5edcdc5489b3
yamadapc <tacla.yamada@gmail.com>
2016-05-31 11:24:05 -0300

    Set LineBuffering on stack-run-auto

--- stack-run-auto/app/Main.hs
+++ stack-run-auto/app/Main.hs
@@ -2,10 +2,13 @@ module Main where
 
 import           StackRunAuto
 import           System.Environment (getArgs)
+import           System.IO
 
 main :: IO ()
 main = do
     args <- getArgs
+    hSetBuffering stdout LineBuffering
+    hSetBuffering stderr LineBuffering
     case args of
         [] -> error "Usage: stack-run-auto <file>"
         (fname:_) -> run (Options fname)
